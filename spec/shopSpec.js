cf4d16671c63f1fd53076f6b3882f1a54d206e0d
John Newman <newmanj903@gmail.com>
2018-06-20 17:15:18 +0100

    Unit Tests made for tickets

--- spec/shopSpec.js
+++ spec/shopSpec.js
@@ -5,6 +5,7 @@ describe("Shop", function(){
     normalItem = {name: "normalItem", sellIn: 1, quality: 20 }
     sulfuras = {name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 }
     agedBrie = {name: "Aged Brie", sellIn: 2, quality: 5}
+    ticket = {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 30 }
   });
 
   describe("#add", function(){
@@ -95,6 +96,47 @@ describe("Shop", function(){
         agedBrie.quality = 50;
         shop.updateQuality();
         expect(agedBrie.quality).toEqual(50);
+      });
+    });
+
+    describe("ticket", function(){
+
+      beforeEach(function(){
+        shop.add(ticket);
+      });
+
+      it("reduces sellIn by 1", function(){
+        shop.updateQuality();
+        expect(ticket.sellIn).toEqual(10);
+      })
+
+      it("increases quality by 1 when sellIn is above 10", function(){
+        shop.updateQuality();
+        expect(ticket.quality).toEqual(31);
+      });
+
+      it("Increase the quality by 2 when sellIn is between 5 and 10", function(){
+        ticket.sellIn = 6;
+        shop.updateQuality();
+        expect(ticket.quality).toEqual(32);
+      });
+
+      it("Increase the quality by 3 when sellIn is between 1 and 5", function(){
+        ticket.sellIn = 2;
+        shop.updateQuality();
+        expect(ticket.quality).toEqual(33);
+      });
+
+      it("Quality equal zero when sellIn is 0 or less", function(){
+        ticket.sellIn = 0;
+        shop.updateQuality();
+        expect(ticket.quality).toEqual(0);
+      });
+
+      it("quality will not go above 50", function(){
+        ticket.quality = 50;
+        shop.updateQuality();
+        expect(ticket.quality).toEqual(50);
       });
     });
   });
