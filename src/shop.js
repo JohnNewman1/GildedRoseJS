9e6b0dc0856a3dd09e13bfbce1b893e101828c85
John Newman <newmanj903@gmail.com>
2018-06-20 15:40:29 +0100

    Test passed for quality not going below 0

--- src/shop.js
+++ src/shop.js
@@ -11,12 +11,16 @@ class Shop {
    this.items.forEach(function(item){
      var n = -1
      if (this._ifSellInbelowZero(item)) { item.quality += n}
+     this._ifQualitybelowZero(item) ? item.quality = 0 : item.quality += n;
      item.sellIn --
-     item.quality += n
    }.bind(this));
  }
 
  _ifSellInbelowZero(item){
    return (item.sellIn <= 0)
+ }
+
+ _ifQualitybelowZero(item){
+   return (item.quality <= 0)
  }
 }
