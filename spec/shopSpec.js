064f398372a3b7c095e63e972b8ed4d85316fac0
John Newman <newmanj903@gmail.com>
2018-06-20 15:00:16 +0100

    Unit test for testing a normal item

--- spec/shopSpec.js
+++ spec/shopSpec.js
@@ -1,14 +1,31 @@
-describe('Shop', function(){
+describe("Shop", function(){
   var shop;
   beforeEach(function(){
     shop = new Shop();
     item = jasmine.createSpy("item");
   });
 
-  describe('#add', function(){
-    it('adds a new item to the item array', function(){
+  describe("#add", function(){
+    it("adds a new item to the item array", function(){
       shop.add(item);
       expect(shop.items).toContain(item)
+    });
+  });
+
+  describe("updateQuality", function(){
+
+    describe("NormalItems", function(){
+      beforeEach(function(){
+        shop.add(item);
+        item.name = "normalItem";
+        item.sellIn = 2;
+        item.quality = 20;
+      });
+
+      it("reduces sellIn by 1", function(){
+        shop.updateQuality();
+        expect(item.sellIn).toEqual(1)
+      })
     });
   });
 });
