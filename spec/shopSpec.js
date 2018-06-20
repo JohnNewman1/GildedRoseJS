aa6d83ba65130db1da5a76d3ec7e3c0bce582ff9
John Newman <newmanj903@gmail.com>
2018-06-20 16:40:34 +0100

    Sulfarus tests passed, mocks had to be changed as spys did not like item.name

--- spec/shopSpec.js
+++ spec/shopSpec.js
@@ -2,14 +2,14 @@ describe("Shop", function(){
   var shop;
   beforeEach(function(){
     shop = new Shop();
-    item = jasmine.createSpy("item");
-    sulfuras = jasmine.createSpy("Sulfuras");
+    normalItem = {name: "normalItem", sellIn: 1, quality: 20 }
+    sulfuras = {name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 }
   });
 
   describe("#add", function(){
     it("adds a new item to the item array", function(){
-      shop.add(item);
-      expect(shop.items).toContain(item)
+      shop.add(normalItem);
+      expect(shop.items).toContain(normalItem)
     });
   });
 
@@ -18,44 +18,39 @@ describe("Shop", function(){
     describe("NormalItems", function(){
 
       beforeEach(function(){
-        shop.add(item);
-        item.name = "normalItem";
-        item.sellIn = 1;
-        item.quality = 20;
+        shop.add(normalItem);
       });
 
       it("reduces sellIn by 1", function(){
         shop.updateQuality();
-        expect(item.sellIn).toEqual(0);
+        expect(normalItem.sellIn).toEqual(0);
       })
 
       it("reduces quality by 1", function(){
         shop.updateQuality();
-        expect(item.quality).toEqual(19);
+        expect(normalItem.quality).toEqual(19);
       });
 
       it("reduces quality by 2 when sellIn below zero", function(){
-        item.sellIn = 0;
+        normalItem.sellIn = 0;
         shop.updateQuality();
-        expect(item.quality).toEqual(18);
+        expect(normalItem.quality).toEqual(18);
       });
       it(" quality will not go below zero", function(){
-        item.quality = 0;
+        normalItem.quality = 0;
         shop.updateQuality();
-        expect(item.quality).toEqual(0);
+        expect(normalItem.quality).toEqual(0);
       });
     });
 
     describe("Sulfuras", function(){
-
       beforeEach(function(){
         shop.add(sulfuras);
-        sulfuras.name = "Sulfuras, Hand of Ragnaros";
-        sulfuras.sellIn = 0;
-        sulfuras.quality = 80;
+        console.log(sulfuras.name);
       });
 
       it("reduces sellIn by 0", function(){
+        console.log(shop._ifSulfurus(sulfuras))
         shop.updateQuality();
         expect(sulfuras.sellIn).toEqual(0);
       })
@@ -68,11 +63,8 @@ describe("Shop", function(){
       it("reduces quality by 0 when sellIn below zero", function(){
         sulfuras.sellIn = -2;
         shop.updateQuality();
-        expect(item.quality).toEqual(80);
+        expect(sulfuras.quality).toEqual(80);
       });
-
-    });
-
     });
   });
 });
--- src/shop.js
+++ src/shop.js
@@ -9,15 +9,20 @@ class Shop {
 
  updateQuality(){
    this.items.forEach(function(item){
+     if (this._ifSulfurus(item)) {return;}
      var n = -1
-     item.sellIn --
      if (this._ifSellInBelowZero(item)) { item.quality += n; }
      this._ifQualityBelowZero(item) ? item.quality = 0 : item.quality += n;
+     item.sellIn --
    }.bind(this));
  }
 
  _ifSellInBelowZero(item){
    return (item.sellIn <= 0)
+ }
+
+ _ifSulfurus(item){
+   return (item.name == "Sulfuras, Hand of Ragnaros")
  }
 
  _ifQualityBelowZero(item){
